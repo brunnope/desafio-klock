6907ecf57071cbfa70a62cb28ce8e335b493a6b1
Brunno Pereira <cicerobrnn111@gmail.com>
2025-05-21 19:32:45 -0300

    feat: Criar classe para testes

---
+++ src/main/java/com/klock/desafio/config/TestConfig.java
@@ -0,0 +1,25 @@
+package com.klock.desafio.config;
+
+import com.klock.desafio.controllers.ClienteController;
+import com.klock.desafio.entities.Cliente;
+import com.klock.desafio.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Cliente cliente = new Cliente("cicero2", "cicero121@gmail.com");
+
+        clienteRepository.save(cliente);
+
+    }
+}
