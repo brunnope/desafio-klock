a1ca499ad44d77ebe201697a5f2ad8f8bc22548c
Brunno Pereira <cicerobrnn111@gmail.com>
2025-05-21 17:02:57 -0300

    feat(entity): criar projeto

---
+++ src/main/java/com/klock/desafio/DesafioKlockApplication.java
@@ -0,0 +1,13 @@
+package com.klock.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioKlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioKlockApplication.class, args);
+	}
+
+}
